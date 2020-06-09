38a85e371ea6f0a61d09904398177bcb46a7792c
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-09 19:07:37 +0800

    improvements in displaying(added completed and delete)

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,16 +1,28 @@
 import React, { Fragment } from 'react';
 import '../assets/css/listComponent.css';
 
-const ListComponent = (props) => {
+const ListComponent = ({data, taskList, setTaskList}) => {
+	const deleteTask = (id) => {
+		setTaskList(taskList.filter(item => item.id !== id))
+	}
+	const completeTask = (id) => {
+		setTaskList([...taskList].map(item => {
+			if(item.id === id) {
+				return {
+					...item,
+					active: !item.active
+				}
+			}
+			return item
+		}))
+	}
 	return (
 		<Fragment>
-			{ 
-				props.task.map(val => 
-					(<div className="box">
-						{val}
-					</div>)
-				)
-			}
+			<div className="box">
+				<input type="checkbox" name="task" />
+				<label className={ data.active ? 'active' : 'passive'} onClick={() => completeTask(data.id)} htmlFor="task">{data.task} {data.id}</label>
+				<button className="danger-bg" onClick={() => deleteTask(data.id)}>Delete</button>
+			</div>
 		</Fragment>
 	)
 }
